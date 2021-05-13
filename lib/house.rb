6dc742cf757e1889419e7e48999ca645f5fde8b7
Brayden Brown <braydenbrown@gmail.com>
2021-05-13 11:33:19 -0700

    Moved recite to top of class for clarity and added an initialize method

--- lib/house.rb
+++ lib/house.rb
@@ -1,4 +1,12 @@
 class House
+
+  def initialize
+  end
+
+  def recite
+    (1..12).collect {|i| line(i)}.join("\n")
+  end
+
   def line(line)
     case line
     when 1
@@ -26,9 +34,5 @@ class House
     when 12
       "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     end
-  end
-
-  def recite
-    (1..12).collect {|i| line(i)}.join("\n")
   end
 end
