2c8d894eabed3f232393c12b3014ad67c6144fde
Brayden Brown <braydenbrown@gmail.com>
2021-05-13 14:58:31 -0700

    Remove first and second case and change parameter name to make more sense and add a case statement to segment

--- lib/house.rb
+++ lib/house.rb
@@ -7,12 +7,8 @@ class House
     (1..12).collect {|i| line(i)}.join("\n")
   end
 
-  def line(line)
-    case line
-    when 1
-      "This is #{segment(1)}the house that Jack built.\n"
-    when 2
-      "This is #{segment(2)}the house that Jack built.\n"
+  def line(line_num)
+    case line_num
     when 3
       "This is the rat that ate the malt that lay in the house that Jack built.\n"
     when 4
@@ -33,14 +29,17 @@ class House
       "This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     when 12
       "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+    else
+      "This is #{segment(line_num)}the house that Jack built.\n"
     end
+
   end
 
-  def segment(number)
-    case number
+  def segment(line_num)
+    case line_num
     when 1
       ""
-    else
+    when 2
       "the malt that lay in "
     end
   end
