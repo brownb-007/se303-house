478f1ade746ad637fc1da0652865365cbfe37efe
Brayden Brown <braydenbrown@gmail.com>
2021-05-13 19:31:34 -0700

    replaced 3rd line statement with call to the method

--- lib/house.rb
+++ lib/house.rb
@@ -10,7 +10,7 @@ class House
   def line(line_num)
     case line_num
     when 3
-      "This is the rat that ate the malt that lay in the house that Jack built.\n"
+      "This is #{segment(line_num)}the house that Jack built.\n"
     when 4
       "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     when 5
