97b97d0c5511eeacc36ee5077d75a56fdf52ec06
Brayden Brown <braydenbrown@gmail.com>
2021-05-14 02:22:26 -0700

    Removed the 4th case in line

--- lib/house.rb
+++ lib/house.rb
@@ -9,8 +9,6 @@ class House
 
   def line(line_num)
     case line_num
-    when 4
-      "This is #{segment(line_num)}the house that Jack built.\n"
     when 5
       "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     when 6
