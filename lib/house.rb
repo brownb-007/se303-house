602861778e74282e5176ead5d09b2f276df3959f
Brayden Brown <braydenbrown@gmail.com>
2021-05-14 02:21:48 -0700

    replaced the segment with the call to the segment method

--- lib/house.rb
+++ lib/house.rb
@@ -10,7 +10,7 @@ class House
   def line(line_num)
     case line_num
     when 4
-      "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+      "This is #{segment(line_num)}the house that Jack built.\n"
     when 5
       "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     when 6
