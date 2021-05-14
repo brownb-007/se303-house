20e9665c18f4067220718127e0f0fe4ef7926477
Brayden Brown <braydenbrown@gmail.com>
2021-05-14 02:21:03 -0700

    created case for the 4th line_num segment

--- lib/house.rb
+++ lib/house.rb
@@ -41,6 +41,8 @@ class House
       "the malt that lay in "
     when 3
       "the rat that ate the malt that lay in "
+    when 4
+      "the cat that killed the rat that ate the malt that lay in "
     end
   end
 end
