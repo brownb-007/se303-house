0699d5a1e12530c20dc511f46070c5b4fbba8b18
Brayden Brown <braydenbrown@gmail.com>
2021-05-14 19:18:02 -0700

    recursivly called segment for the previous line of the song and created function verses which will end up being an array of the verses to call

--- lib/house.rb
+++ lib/house.rb
@@ -16,7 +16,7 @@ class House
     when 1
       "the house that Jack built.\n"
     when 2
-      "the malt that lay in "
+      "the malt that lay in #{segment(line_num-1)}"
     when 3
       "the rat that ate the malt that lay in "
     when 4
@@ -38,5 +38,11 @@ class House
     when 12
       "the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
     end
+  end
+
+  def verses
+    [
+      ""
+    ]
   end
 end
