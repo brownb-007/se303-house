3046e5828c934146d15fcbfba8c6e897f7d806c1
Brayden Brown <braydenbrown@gmail.com>
2021-05-16 15:36:51 -0700

    Removed segment method and trailing whitespace in verses function

--- lib/house.rb
+++ lib/house.rb
@@ -8,31 +8,24 @@ class House
   end
 
   def line(line_num)
-    "This is #{verses.last(line_num).join("")}"
+    "This is #{verses.last(line_num).join(" ")}"
   end
 
-  def segment(line_num)
-    if line_num == 0
-      ""
-    else
-      "#{verses[line_num-1]}#{segment(line_num-1)}"
-    end
-  end
 
   def verses
     [
       "the house that Jack built.\n",
-      "the malt that lay in ",
-      "the rat that ate ",
-      "the cat that killed ",
-      "the dog that worried ",
-      "the cow with the crumpled horn that tossed ",
-      "the maiden all forlorn that milked ",
-      "the man all tattered and torn that kissed ",
-      "the priest all shaven and shorn that married ",
-      "the rooster that crowed in the morn that woke ",
-      "the farmer sowing his corn that kept ",
-      "the horse and the hound and the horn that belonged to "
+      "the malt that lay in",
+      "the rat that ate",
+      "the cat that killed",
+      "the dog that worried",
+      "the cow with the crumpled horn that tossed",
+      "the maiden all forlorn that milked",
+      "the man all tattered and torn that kissed",
+      "the priest all shaven and shorn that married",
+      "the rooster that crowed in the morn that woke",
+      "the farmer sowing his corn that kept",
+      "the horse and the hound and the horn that belonged to"
     ].reverse
   end
 end
