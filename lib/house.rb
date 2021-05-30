476b8a38f6c5026485eae61fae090f6c2387c2f4
Brayden Brown <braydenbrown@gmail.com>
2021-05-29 17:11:06 -0700

    Fixed whitespace and quatation

--- lib/house.rb
+++ lib/house.rb
@@ -1,33 +1,32 @@
 class House
-
   def recite
-    (1..verses.length).collect {|i| line(i)}.join("\n")
+    (1..verses.length).collect { |i| line(i) }.join("\n")
   end
 
   def recite_randomly
-    (1..verses.length).collect {|i| random_line(i)}.join("\n")
+    (1..verses.length).collect { |i| random_line(i) }.join("\n")
   end
 
   def recite_with_random_verses
-    (1..verses.length).collect {|i| random_line_verses(i)}.join("\n")
+    (1..verses.length).collect { |i| random_line_verses(i) }.join("\n")
   end
 
   def line(line_num)
-    "#{intro_to_verse} #{verses.reverse.last(line_num).join(" ")}.\n"
+    "#{intro_to_verse} #{verses.reverse.last(line_num).join(' ')}.\n"
   end
 
   def random_line(line_num)
-    "#{intro_to_verse} #{randomize_line_order(line_num).join(" ")}.\n"
+    "#{intro_to_verse} #{randomize_line_order(line_num).join(' ')}.\n"
   end
 
   def random_line_verses(line_num)
-    "#{intro_to_verse} #{randomize_using_random_verses(line_num).join(" ")}.\n"
+    "#{intro_to_verse} #{randomize_using_random_verses(line_num).join(' ')}.\n"
   end
 
   private
 
   def intro_to_verse
-    "This is"
+    'This is'
   end
 
   def randomize_line_order(line_num)
@@ -39,57 +38,55 @@ class House
   end
 
   def verses
-    12.times.map {|i| "the #{subjects[i]} that #{verbs[i]}"}
+    12.times.map { |i| "the #{subjects[i]} that #{verbs[i]}" }
   end
 
   def random_verses
-    12.times.map {|i| "the #{subjects.sample} that #{verbs.sample}"}
+    12.times.map { |_i| "the #{subjects.sample} that #{verbs.sample}" }
   end
 
   def subjects
     [
-      "house",
-      "malt",
-      "rat",
-      "cat",
-      "dog",
-      "cow with the crumpled horn",
-      "maiden all forlorn",
-      "man all tattered and torn",
-      "priest all shaven and shorn",
-      "rooster that crowed in the morn",
-      "farmer sowing his corn",
-      "horse and the hound and the horn",
+      'house',
+      'malt',
+      'rat',
+      'cat',
+      'dog',
+      'cow with the crumpled horn',
+      'maiden all forlorn',
+      'man all tattered and torn',
+      'priest all shaven and shorn',
+      'rooster that crowed in the morn',
+      'farmer sowing his corn',
+      'horse and the hound and the horn'
     ]
   end
 
   def verbs
     [
-      "Jack built",
-      "lay in",
-      "ate",
-      "killed",
-      "worried",
-      "tossed",
-      "milked",
-      "kissed",
-      "married",
-      "woke",
-      "kept",
-      "belonged to",
+      'Jack built',
+      'lay in',
+      'ate',
+      'killed',
+      'worried',
+      'tossed',
+      'milked',
+      'kissed',
+      'married',
+      'woke',
+      'kept',
+      'belonged to'
     ]
   end
-
 end
 
 class PirateHouse < House
-  private 
+  private
 
   def intro_to_verse
-    "Thar be"
+    'Thar be'
   end
 end
-
 
 puts House.new.recite_randomly
 puts "\n\n"
